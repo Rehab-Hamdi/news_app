79249b9855d68d3203861ba137e3978211fe14fa
Rehab Hamdy <80859649+Rehab-Hamdi@users.noreply.github.com>
2025-02-18 17:47:04 +0200

    data

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+import 'package:news_app/models/category_model.dart';
+
+List<CategoryModel> categories = [
+  CategoryModel(image: 'assets/images/science.jpeg', name: 'Science'),
+  CategoryModel(image: 'assets/images/business.jpeg', name: 'Business'),
+  CategoryModel(image: 'assets/images/general.jpeg', name: 'General'),
+  CategoryModel(image: 'assets/images/health.jpeg', name: 'Health'),
+
+  CategoryModel(image: 'assets/images/sport.jpeg', name: 'Sports'),
+  CategoryModel(image: 'assets/images/technology.jpeg', name: 'Technology'),
+  CategoryModel(
+      image: 'assets/images/entertainment.jpeg', name: 'Entertainment'),
+];
